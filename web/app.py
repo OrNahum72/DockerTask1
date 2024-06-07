27f9d00ab04f87a49a1e5ac455608764eb0a02aa
OrNahum <ornahum72@gmail.com>
2024-06-07 10:11:29 +0300

    mynewcommit2

--- web/app.py
+++ web/app.py
@@ -7,7 +7,7 @@ def home():
     return f"Hello, class!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 app = Flask(__name__)
 
